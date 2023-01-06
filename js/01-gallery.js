dd8d315af6a435a2cfdc11ecec780223e93eb236
Yevhen Liaskivskyi <benatanlev@icloud.com>
2023-01-06 13:56:51 +0200

    All done hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,11 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// Gallery markup
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 const markup = galleryItems
-    .map((item) =>
-        `<div class="gallery__item">
+  .map(
+    (item) =>
+      `<div class="gallery__item">
             <a class="gallery__link" href="${item.original}">
                 <img
                     class="gallery__image"
@@ -14,20 +14,22 @@ const markup = galleryItems
                     alt="${item.description}"
                 />
             </a>
-        </div>`)
-    .join('');
+        </div>`
+  )
+  .join("");
 gallery.innerHTML = markup;
 
-// Click event handle
-let bigSizeUrl = '';
-gallery.addEventListener('click', handleClick);
+let bigSizeUrl = "";
+gallery.addEventListener("click", handleClick);
 function handleClick(event) {
-    event.preventDefault();
-    bigSizeUrl = event.target.dataset.source;
-    const instance = basicLightbox.create(`<img src=${bigSizeUrl}>`);
-    instance.show(() => document.body.addEventListener("keydown", ({key}) => {
-        if (key === "Escape") {
-        instance.close()
-        }
-    }));
+  event.preventDefault();
+  bigSizeUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(`<img src=${bigSizeUrl}>`);
+  instance.show(() =>
+    document.body.addEventListener("keydown", ({ key }) => {
+      if (key === "Escape") {
+        instance.close();
+      }
+    })
+  );
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,25 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// Gallery markup
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 const markup = galleryItems
-    .map((item) =>
-        `<a class="gallery__item" href="${item.original}">
+  .map(
+    (item) =>
+      `<a class="gallery__item" href="${item.original}">
             <img
                 class="gallery__image"
                 src="${item.preview}"
                 alt="${item.description}"
             />
-        </a>`)
-    .join('');
+        </a>`
+  )
+  .join("");
 gallery.innerHTML = markup;
 
-// Click event handle
-gallery.addEventListener('click', handleClick);
-const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
+gallery.addEventListener("click", handleClick);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 function handleClick(event) {
-    event.preventDefault();
-    const element = event.target;
-    lightbox.open(element);
+  event.preventDefault();
+  const element = event.target;
+  lightbox.open(element);
 }
