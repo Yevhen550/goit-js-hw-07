34203969fbcba6a586f989a1e0a763c51b7e2bc6
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-11-22 19:45:21 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,9 +1,39 @@
+const boxEl = document.querySelector('#boxes');
+const inputEl = document.querySelector('input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+
+let boxSize = 30;
+
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const sum = +(amount.value = inputEl.value);
+  let newArr = [];
+
+  for (let i = 0; i < sum; i++) {
+    boxSize += 10;
+
+    const newDiv = document.createElement('div');
+    const color = getRandomHexColor();
+    newDiv.style.backgroundColor = color;
+    newDiv.style.width = `${boxSize}px`;
+    newDiv.style.height = `${boxSize}px`;
+
+    newArr.push(newDiv);
+  }
+  boxEl.append(...newArr);
+}
+
+function destroyBoxes() {
+  boxEl.innerHTML = '';
+  inputEl.value = '';
+  boxSize = 20;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
-
-function createBoxes(params) {
-  console.log('lol');
 }
