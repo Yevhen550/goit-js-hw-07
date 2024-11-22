e7c9c250c5e7a8bbc10ca62993624946f0cfb4ba
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-11-22 15:15:53 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+const onFormSubmit = ev => {
+  ev.preventDefault();
+
+  const formDate = {
+    [form.elements.email.name]: form.elements.email.value.trim(),
+    [form.elements.password.name]: form.elements.password.value.trim(),
+  };
+
+  if (form.elements.email.value === '' || form.elements.password.value === '') {
+    alert('All form fields must be filled in');
+  }
+
+  console.log(formDate);
+
+  form.reset();
+};
+
+form.addEventListener('submit', onFormSubmit);
